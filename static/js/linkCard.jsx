70a0e2985e7e29a9561800c2ae0075e897b93023
Erin Taylor <erin@entaylor.io>
2021-10-25 22:00:32 +0000

    Created modal pop up for edit button on the link card div

--- static/js/linkCard.jsx
+++ static/js/linkCard.jsx
@@ -1,13 +1,106 @@
+// Link card function to create a card of the link(s) added.
 function LinkCard(props) {
+  const [isModalVisible, setIsModalVisible] = React.useState(false);
   return (
     <div className="card">
       <img src={props.image} alt="Image not available"/>
       <p><a href="{props.link_path}">{props.name}</a></p>
       <p> Notes: {props.notes} </p>
+      <button onClick={() => setIsModalVisible(true)}>Edit</button>
+      {isModalVisible && (
+        <Modal onModalClose={() => setIsModalVisible(false)}>
+          <Modal.Header>Edit Card</Modal.Header>
+          <Modal.Body>Body</Modal.Body>
+          <Modal.Footer>
+            <Modal.Footer.CloseBtn>Close</Modal.Footer.CloseBtn>
+          </Modal.Footer>
+        </Modal>
+      )}
     </div>
     );
 }
+// More modal functions for editing the link cards.
+const modalContext = React.createContext();
 
+function Modal({ children, onModalClose }) {
+  React.useEffect(() => {
+    function keyListener(e) {
+      const listener = keyListenersMap.get(e.keyCode);
+      return listener && listener(e);
+    }
+    document.addEventListener("keydown", keyListener);
+
+    return () => document.removeEventListener("keydown", keyListener);
+  });
+
+  const modalRef = React.createRef();
+  const handleTabKey = e => {
+    const focusableModalElements = modalRef.current.querySelectorAll(
+      'a[href], button, textarea, input[type="text"], input[type="radio"], input[type="checkbox"], select'
+    );
+    const firstElement = focusableModalElements[0];
+    const lastElement =
+      focusableModalElements[focusableModalElements.length - 1];
+
+    if (!e.shiftKey && document.activeElement !== firstElement) {
+      firstElement.focus();
+      return e.preventDefault();
+    }
+
+    if (e.shiftKey && document.activeElement !== lastElement) {
+      lastElement.focus();
+      e.preventDefault();
+    }
+  };
+
+  const keyListenersMap = new Map([[27, onModalClose], [9, handleTabKey]]);
+
+  return ReactDOM.createPortal(
+    <div className="modal-container" role="dialog" aria-modal="true">
+      <div className="modal-content" ref={modalRef}>
+        <modalContext.Provider value={{ onModalClose }}>
+          {children}
+        </modalContext.Provider>
+      </div>
+    </div>,
+    document.body
+  );
+}
+
+Modal.Header = function ModalHeader(props) {
+  const { onModalClose } = React.useContext(modalContext);
+
+  return (
+    <div className="modal-header">
+      {props.children}
+      <button className="cross-btn" title="close modal" onClick={onModalClose}>
+        ✕
+      </button>
+    </div>
+  );
+};
+
+Modal.Body = function ModalBody(props) {
+  return <div className="modal-body">{props.children}</div>;
+};
+
+Modal.Footer = function ModalFooter(props) {
+  return <div className="modal-footer">{props.children}</div>;
+};
+
+Modal.Footer.CloseBtn = function CloseBtn(props) {
+  const { onModalClose } = React.useContext(modalContext);
+  return (
+    <button
+      {...props}
+      className="close-btn"
+      title="close modal"
+      onClick={onModalClose}
+    />
+  );
+};
+
+// Creating the container of Link cards
 function LinkCardContainer() {
   const [cards, setCards] = React.useState([]);
 
